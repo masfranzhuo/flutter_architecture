a5586723b7ffecd35d368c1edeefda704851e2cb
masfranzhuo <masfranzhuo@gmail.com>
2020-04-20 20:58:45 +0700

    remove warning error cause by account models

--- lib/features/account/data/models/customer_model.dart
+++ lib/features/account/data/models/customer_model.dart
@@ -1,40 +1,40 @@
-import 'package:flutter_architecture/features/account/domain/entities/customer.dart';
-import 'package:flutter_architecture/core/util/json_parsers/date_time.dart';
-import 'package:meta/meta.dart';
-import 'package:json_annotation/json_annotation.dart';
+// import 'package:flutter_architecture/features/account/domain/entities/customer.dart';
+// import 'package:flutter_architecture/core/util/json_parsers/date_time.dart';
+// import 'package:meta/meta.dart';
+// import 'package:json_annotation/json_annotation.dart';
 
-part 'customer_model.g.dart';
+// part 'customer_model.g.dart';
 
-@Deprecated('unused model in data layer, use entity in domain layer instead')
-@JsonSerializable()
-class CustomerModel extends Customer {
-  const CustomerModel({
-    @required String id,
-    @required String name,
-    @required String email,
-    @required String accountStatus,
-    String phoneNumber,
-    String photoUrl,
-    String gender,
-    String birthPlace,
-    DateTime birthDate,
-  }) : super(
-          id: id,
-          name: name,
-          email: email,
-          accountStatus: accountStatus,
-          phoneNumber: phoneNumber,
-          photoUrl: photoUrl,
-          gender: gender,
-          birthPlace: birthPlace,
-          birthDate: birthDate,
-        );
+// @Deprecated('unused model in data layer, use entity in domain layer instead')
+// @JsonSerializable()
+// class CustomerModel extends Customer {
+//   const CustomerModel({
+//     @required String id,
+//     @required String name,
+//     @required String email,
+//     @required String accountStatus,
+//     String phoneNumber,
+//     String photoUrl,
+//     String gender,
+//     String birthPlace,
+//     DateTime birthDate,
+//   }) : super(
+//           id: id,
+//           name: name,
+//           email: email,
+//           accountStatus: accountStatus,
+//           phoneNumber: phoneNumber,
+//           photoUrl: photoUrl,
+//           gender: gender,
+//           birthPlace: birthPlace,
+//           birthDate: birthDate,
+//         );
 
-  factory CustomerModel.fromJson(Map<String, dynamic> json) =>
-      _$CustomerModelFromJson(json);
+//   factory CustomerModel.fromJson(Map<String, dynamic> json) =>
+//       _$CustomerModelFromJson(json);
 
-  Map<String, dynamic> toJson() => _$CustomerModelToJson(this);
+//   Map<String, dynamic> toJson() => _$CustomerModelToJson(this);
 
-  @override
-  String toString() => toJson().toString();
-}
+//   @override
+//   String toString() => toJson().toString();
+// }
--- test/features/account/data/models/staff_model_test.dart
+++ test/features/account/data/models/staff_model_test.dart
@@ -1,50 +1,50 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:flutter_architecture/features/account/data/models/staff_model.dart';
-import 'package:flutter_architecture/features/account/domain/entities/account.dart';
-import 'package:flutter_architecture/features/account/domain/entities/staff.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_architecture/features/account/data/models/staff_model.dart';
+// import 'package:flutter_architecture/features/account/domain/entities/account.dart';
+// import 'package:flutter_architecture/features/account/domain/entities/staff.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import '../../../../fixtures/fixtures_reader.dart';
+// import '../../../../fixtures/fixtures_reader.dart';
 
-void main() {
-  Map<String, dynamic> staffModelFixture;
+// void main() {
+//   Map<String, dynamic> staffModelFixture;
 
-  setUp(() {
-    staffModelFixture = Map<String, dynamic>.from(
-      json.decode(fixture('fixtures/staffs/complete_valid.json')),
-    );
-  });
+//   setUp(() {
+//     staffModelFixture = Map<String, dynamic>.from(
+//       json.decode(fixture('fixtures/staffs/complete_valid.json')),
+//     );
+//   });
 
-  final staffModelTest = StaffModel(
-    id: 'fake_id',
-    name: 'John Doe',
-    email: 'john@doe.com',
-    accountStatus: AccountStatus.active,
-    role: StaffRole.admin,
-    phoneNumber: '1234567890',
-    photoUrl: 'https://fakeimage.com/image.jpg',
-  );
+//   final staffModelTest = StaffModel(
+//     id: 'fake_id',
+//     name: 'John Doe',
+//     email: 'john@doe.com',
+//     accountStatus: AccountStatus.active,
+//     role: StaffRole.admin,
+//     phoneNumber: '1234567890',
+//     photoUrl: 'https://fakeimage.com/image.jpg',
+//   );
 
-  test('should be a Staff entity', () async {
-    expect(staffModelTest, isA<Staff>());
-  });
+//   test('should be a Staff entity', () async {
+//     expect(staffModelTest, isA<Staff>());
+//   });
 
-  group('fromJson', () {
-    test('should return a valid staff model', () async {
-      final result = StaffModel.fromJson(staffModelFixture);
+//   group('fromJson', () {
+//     test('should return a valid staff model', () async {
+//       final result = StaffModel.fromJson(staffModelFixture);
 
-      expect(result, staffModelTest);
-    });
-  });
+//       expect(result, staffModelTest);
+//     });
+//   });
 
-  group('toJson', () {
-    test('should return a JSON map', () async {
-      final result = staffModelTest.toJson();
+//   group('toJson', () {
+//     test('should return a JSON map', () async {
+//       final result = staffModelTest.toJson();
 
-      final expectedMap = staffModelFixture;
+//       final expectedMap = staffModelFixture;
 
-      expect(result, expectedMap);
-    });
-  });
-}
+//       expect(result, expectedMap);
+//     });
+//   });
+// }

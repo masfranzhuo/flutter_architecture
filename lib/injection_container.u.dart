dfe2354182e852d31fdb8f67b1e71eecbfe0d201
masfranzhuo <masfranzhuo@gmail.com>
2020-06-19 16:52:29 +0700

    fix di

--- lib/injection_container.u.dart
+++ lib/injection_container.u.dart
@@ -2,6 +2,8 @@ import 'package:flutter_architecture/features/account/account_injection_containe
     as accountDI;
 import 'package:flutter_architecture/features/storage/storage_injection_container.dart'
     as storageDI;
+import 'package:flutter_architecture/features/users_overview/users_overview_injection_container.dart'
+    as usersOverviewDI;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
@@ -10,6 +12,7 @@ import 'core/util/bloc_delegate.dart';
 void init() {
   accountDI.init();
   storageDI.init();
+  usersOverviewDI.init();
 
   // External
   BlocSupervisor.delegate = FlutterBlocDelegate();

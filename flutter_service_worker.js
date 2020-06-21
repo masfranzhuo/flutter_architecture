b64b115d98dc84df7615f3f06b743f8f890774c4
masfranzhuo <masfranzhuo@gmail.com>
2020-06-21 09:27:21 +0000

    Deploying to gh-pages from master @ 84c696f7333934fdb3e85a8614bd035ae2d523ca 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,17 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "f52bd675f86b0e106fdc36b4abfaca0f",
-"main.dart.js": "6d26f0b52476189b92268323194613c7",
+  "main.dart.js": "f4a4da718a3d76a4b7084e6bc7c7f621",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "525fc4e70d59ac813d4db8ca4cd551d5",
-"assets/LICENSE": "a1a16e485514393d47f91ad50f157996",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/LICENSE": "3cef734df4bc15aed9a85a958d1f91d1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"index.html": "f52bd675f86b0e106fdc36b4abfaca0f"
 };
 
 self.addEventListener('activate', function (event) {
